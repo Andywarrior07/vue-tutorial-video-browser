8962771ce91f1c4226b49393dc78af38efb84cbb
andresguerrero07 <andres@legalbot.cl>
2021-02-08 17:17:05 -0300

    first commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" placeholder="Search..." @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput({ target }) {
+      const { value } = target;
+
+      this.$emit('termChange', value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  width: 75%;
+}
+
+div {
+  text-align: center;
+  margin: 20px;
+}
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="list-group col-md-4">
+    <VideoListItem
+      v-for="video in videos"
+      :key="video.etag"
+      :video="video"
+      @videoSelected="onVideoSelected"
+    ></VideoListItem>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+
+export default {
+  name: 'VideoList',
+  props: {
+    videos: Array,
+  },
+  components: {
+    VideoListItem,
+  },
+  methods: {
+    onVideoSelected(video) {
+      this.$emit('videoSelected', video);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
